f79733394a6e3cf467846922d60e17d10f409d76
Chanha Kim <chachababy@chanhas-mbp.mynetworksettings.com>
2021-10-15 12:05:38 -0400

    dd

--- Processor.cpp
+++ Processor.cpp
@@ -383,7 +383,7 @@ void Processor::flush_input_buf() {
         assert(msg_received_map.count(i) == 1);
         std::cout << "About to write to file " << std::endl;
         std::cout << "Message about to be written: id: " << msg_received_map[i]->machine_id << " pkt_idx: " << msg_received_map[i]->pkt_idx << " rand: " << msg_received_map[i]->random_num << std::endl;
-        int bytes_written = fprintf(fp, "%2d, %8d, %8d\n", msg_received_map[i]->machine_id, msg_received_map[i]->pkt_idx, msg_received_map[i]->random_num);
+        int bytes_written = fprintf(fp, "%2d, %8d, %8d\n", (int)msg_received_map[i]->machine_id, (int)msg_received_map[i]->pkt_idx, (int)msg_received_map[i]->random_num);
         std::cout << "Bytes Written to the File: " << bytes_written << std::endl;
         msg_received_map.erase(i);
         //find stack
