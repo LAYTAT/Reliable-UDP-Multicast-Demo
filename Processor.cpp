2a48f966fb069dedebc55663bac0447600828e15
Chanha Kim <chachababy@chanhas-mbp.mynetworksettings.com>
2021-10-15 11:39:14 -0400

    ddd

--- Processor.cpp
+++ Processor.cpp
@@ -382,6 +382,7 @@ void Processor::flush_input_buf() {
         // i is the sequence number we can write into, i.e. we can find it from the msg_recieved!
         assert(msg_received_map.count(i) == 1);
         std::cout << "About to write to file " << std::endl;
+        std::cout << "Message about to be written: id: " << msg_received_map[i]->machine_id << " pkt_idx: " << msg_received_map[i]->pkt_idx << " rand: " << msg_received_map[i]->random_num << std::endl;
         int bytes_written = fprintf(fp, "%2d, %8d, %8d\n", msg_received_map[i]->machine_id, msg_received_map[i]->pkt_idx, msg_received_map[i]->random_num);
         std::cout << "Bytes Written to the File: " << bytes_written << std::endl;
         msg_received_map.erase(i);
