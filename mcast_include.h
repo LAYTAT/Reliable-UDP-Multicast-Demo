ec1f56d102da4d07914fed4d9fbe0422f9855f35
Chanha Kim <chachababy@chanhas-mbp.mynetworksettings.com>
2021-10-15 08:18:36 -0400

    immadiee

--- mcast_include.h
+++ mcast_include.h
@@ -36,7 +36,7 @@
 #define DATA_SIZE (1400)
 #define MAX_FLOW_FOR_ONE_PROCESS (500)
 #define MAX_FLOW_FOR_ONE_RING_CYCLE (5000)
-#define MAX_RTR (1000)
+#define MAX_RTR (300)
 #define TOKEN_TIMEOUT_GAP_IN_SECONDS (1)
 #define TOEKN_TIMEOUT_GAP_IN_USEC  (100000) //1 / 10 sec
 
@@ -44,9 +44,9 @@ struct Token{
     int seq;                        // The largest sequence number for any message, an upper limit of sequemce number
     int aru;                        // Used to demtermine if all processors on the ring have received all messages up until this number
     int last_aru_setter;            // the machine id of the last processor that have set aru OR the token initiator for ring forming round.
-    unsigned int rtr_size;          // the number of requested
-    unsigned int rtr[MAX_RTR];      // also called nacks, a request list that contains one or more retransmission requiest check vector size for request size
-    unsigned int round;             // the round number in the ring cycle
+    int rtr_size;          // the number of requested
+    int rtr[MAX_RTR];      // also called nacks, a request list that contains one or more retransmission requiest check vector size for request size
+    int round;             // the round number in the ring cycle
     int fcc;                        // for flow control
 };
 
@@ -58,7 +58,7 @@ enum class MSG_TYPE{
 };
 
 struct Message{                     // or called packet
-    MSG_TYPE type;                       // indicate different types of message, join message for example
+    MSG_TYPE type;                       // indicate different types of message, or token for example
     int seq;                        // global packet_index
     int pkt_idx;                    // packet index for each machine
     int machine_id;
