c855a7e429d4fb584b1617dac3a53ae1cc638ec1
LAY <591084988@qq.com>
2021-10-16 12:38:35 -0400

    break bug found

--- Processor.cpp
+++ Processor.cpp
@@ -211,7 +211,7 @@ bool Processor::data_tranfer(){
         }
         case MSG_TYPE::TOKEN: {
             memcpy(token_buf, recv_buf->payload, sizeof(Token));
-            if(token_buf -> round == last_token_round && machine_id != 1) {
+            if(token_buf -> round <= last_token_round && machine_id != 1) {
                 std::cout << "Token Received:       with same last_token_round =" << last_token_round << std::endl;
                 break;
             }
